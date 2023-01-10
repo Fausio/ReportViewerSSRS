e94877cdf850c1f459c10347d05ae6056361c55a
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2023-01-10 08:58:00 +0200

    clean references

---
+++ ReportViewerSSRS/Controllers/ReportController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportViewerSSRS.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index() =>  View();
+         
+    }
+}
